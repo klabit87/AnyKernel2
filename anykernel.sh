025800a5363b16bf032bd84d864fdf746613f570
klabit87 <keith.labit@gmail.com>
2018-12-21 11:09:49 -0600

    Adapt for klabit kernel S9+ (Qualcomm)

--- anykernel.sh
+++ anykernel.sh
@@ -4,18 +4,20 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Flash Kernel for the Pixel 2 (XL) by @nathanchance
+kernel.string=klabit Kernel for the Samsung Galaxy S9+ by @klabit87
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=taimen
-device.name2=walleye
+device.name1=star2qlte
+device.name2=star2qltexx
+device.name3=star2qltezh
+device.name4=star2qltechn
 '; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=1;
+is_slot_device=0;
 ramdisk_compression=auto;
 
 
@@ -36,55 +38,28 @@ dump_boot;
 
 
 # Warn user of their support status
-android_version="$(file_getprop /system/build.prop "ro.build.version.release")";
-security_patch="$(file_getprop /system/build.prop "ro.build.version.security_patch")";
-case "$android_version:$security_patch" in
-  "9:2018-11-05") support_status="a supported";;
-  "8.1.0"*|"P"*|"9"*) support_status="an unsupported";;
-  *) die "Completely unsupported OS configuration!";;
-esac;
-ui_print " "; ui_print "You are on $android_version with the $security_patch security patch level! This is $support_status configuration...";
+#android_version="$(file_getprop /system/build.prop "ro.build.version.release")";
+#security_patch="$(file_getprop /system/build.prop "ro.build.version.security_patch")";
+#case "$android_version:$security_patch" in
+#  "9:2018-11-05") support_status="a supported";;
+#  "8.1.0"*|"P"*|"9"*) support_status="an unsupported";;
+#  *) die "Completely unsupported OS configuration!";;
+#esac;
+#ui_print " "; ui_print "You are on $android_version with the $security_patch security patch level! This is $support_status configuration...";
 
 
 # If the kernel image and dtbs are separated in the zip
 decompressed_image=/tmp/anykernel/kernel/Image
-compressed_image=$decompressed_image.lz4
+compressed_image=$decompressed_image.gz
+ui_print " "; ui_print "Magisk detected! Patching kernel so reflashing Magisk is not necessary...";
 if [ -f $compressed_image ]; then
   # Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
   if [ -d $ramdisk/.backup ]; then
-    ui_print " "; ui_print "Magisk detected! Patching kernel so reflashing Magisk is not necessary...";
     $bin/magiskboot --decompress $compressed_image $decompressed_image;
     $bin/magiskboot --hexpatch $decompressed_image 736B69705F696E697472616D667300 77616E745F696E697472616D667300;
-    $bin/magiskboot --compress=lz4 $decompressed_image $compressed_image;
+    $bin/magiskboot --compress=gzip $decompressed_image $compressed_image;
   fi;
-
-  # Concatenate all of the dtbs to the kernel
-  cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.lz4-dtb;
 fi;
-
-
-# Patch dtbo.img on custom ROMs
-username="$(file_getprop /system/build.prop "ro.build.user")";
-echo "Found user: $username";
-case "$username" in
-  "android-build") user=google;;
-  *) user=custom;;
-esac;
-hostname="$(file_getprop /system/build.prop "ro.build.host")";
-echo "Found host: $hostname";
-case "$hostname" in
-  *corp.google.com|abfarm*) host=google;;
-  *) host=custom;;
-esac;
-if [ "$user" == "custom" -o "$host" == "custom" ]; then
-  if [ ! -z /tmp/anykernel/dtbo.img ]; then
-    ui_print " "; ui_print "You are on a custom ROM, patching dtbo to remove verity...";
-    $bin/magiskboot --dtb-patch /tmp/anykernel/dtbo.img;
-  fi;
-else
-  ui_print " "; ui_print "You are on stock, not patching dtbo to remove verity!";
-fi;
-
 
 # Install the boot image
 write_boot;
