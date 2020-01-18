7c14f7e4f55ab8deccf978018c6b34d674b442a6
osm0sis <osm0sis@outlook.com>
2020-01-18 07:22:09 -0400

    anykernel: add do.systemless

--- anykernel.sh
+++ anykernel.sh
@@ -7,6 +7,7 @@ properties() { '
 kernel.string=klabit Kernel for the Samsung Galaxy S9+ by @klabit87
 do.devicecheck=1
 do.modules=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=star2qlte
