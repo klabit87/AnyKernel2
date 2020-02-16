899c42baa7d9ebc686d0f8aa68324a5adb457200
klabit87 <keith.labit@gmail.com>
2020-02-16 00:12:27 -0600

    Fixup anykernel script for S9+ Android 10

--- anykernel.sh
+++ anykernel.sh
@@ -14,7 +14,7 @@ device.name1=starqlte
 device.name2=starqltexx
 device.name3=starqltezh
 device.name4=starqltechn
-supported.versions=
+supported.versions=9 - 10
 supported.patchlevels=
 '; } # end properties
 
@@ -41,23 +41,12 @@ dump_boot;
 # begin ramdisk changes
 
 # Ramdisk changes - Set split_img OSLevel depending on ROM
-(grep -w ro.build.version.security_patch | cut -d= -f2) </system/system/build.prop > /tmp/rom_oslevel
+(grep -w ro.build.version.security_patch | cut -d= -f2) </system/build.prop > /tmp/rom_oslevel
 ROM_OSLEVEL=`cat /tmp/rom_oslevel`
 echo $ROM_OSLEVEL | rev | cut -c4- | rev > /tmp/rom_oslevel
 ROM_OSLEVEL=`cat /tmp/rom_oslevel`
 ui_print "- Setting security patch level to $ROM_OSLEVEL"
 echo $ROM_OSLEVEL > $split_img/boot.img-oslevel
-
-# Warn user of their support status
-android_version="$(file_getprop /system/system/build.prop "ro.build.version.release")";
-#security_patch="$(file_getprop /system/build.prop "ro.build.version.security_patch")";
-case "$android_version:$security_patch" in
-  "10") support_status="a supported";;
-  "9") support_status="a supported";;
-  "8.1.0") support_status="an unsupported";;
-  *) die "Completely unsupported OS configuration!";;
-esac;
-ui_print " "; ui_print "You are on $android_version ! This is $support_status configuration...";
 
 # If the kernel image and dtbs are separated in the zip
 decompressed_image=/tmp/anykernel/kernel/Image
